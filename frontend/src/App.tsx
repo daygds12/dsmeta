39a974f17be222177222d34a1654a0cebc8653c4
Dayane <you@example.com>
2022-07-13 22:39:08 -0300

    security config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
 
 function App() {
     return (
         <div>
-            <h1>
-                Hello</h1>
+           
+            <Header/>
             <NotificationButton/>
 
         </div>
