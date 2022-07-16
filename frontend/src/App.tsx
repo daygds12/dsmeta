8b1377198a65e119abee45e22b9178f51e97435c
Dayane <you@example.com>
2022-07-16 18:17:34 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,21 @@
 import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
     return (
-        <div>
-           
-            <Header/>
-            <NotificationButton/>
+        <>
 
-        </div>
+            <Header/>
+            <main>
+                <section id="sales">
+                    <div className="dsmeta-container">
+                        <SalesCard/>
+
+                    </div>
+                </section>
+            </main>
+        </>
     )
 }
 
